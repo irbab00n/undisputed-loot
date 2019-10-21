738110088554d2b982a870164c0e934b58d6bc19
Tommy Cosby <cosbyts@gmail.com>
2019-10-20 18:22:09 -0700

    Add Procfile to allow the app to run on Heroku

---
+++ server.js
@@ -0,0 +1,32 @@
+/**
+ * * ----------------------------------------- *
+ * * --- UNDISPUTED LOOT PRODUCTION SERVER --- *
+ * * ----------------------------------------- *
+ * @description
+ * This server is meant to be run inside of the Heroku Node process in order to server the static index.html file.
+ *
+ * This is a quick-hit solution and not the most optomized, but we're not serving 1,000,000+ users :)
+ */
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const port = process.env.PORT || 1337;
+
+const app = express();
+
+const _build = path.join(__dirname, "/build");
+
+app.use(express.static(_build));
+
+// app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/*", (req, res) => {
+  res.sendFile(`${_build}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Undisputed loot application is now listening on port ${port}`);
+});
